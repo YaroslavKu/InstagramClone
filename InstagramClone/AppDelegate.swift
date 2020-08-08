549bed53ea3ebd5d9e87eb658da37db233afe139
Yaroslav Kukhar <kukhar.yr@gmail.com>
2020-08-09 02:55:25 +0300

    install Firebase

--- InstagramClone/AppDelegate.swift
+++ InstagramClone/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Change the color of tabbar's items
+        UITabBar.appearance().tintColor = .black
+        
+        // Firebase configuration
+        FirebaseApp.configure()
         return true
     }
 
